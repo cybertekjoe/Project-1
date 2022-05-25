f838ddccaa336970da8648f276e50ccc271ba6b2
cybertekjoe <105815275+cybertekjoe@users.noreply.github.com>
2022-05-25 00:24:43 -0500

    Delete linux/Player_Dealer_Correlation directory

--- linux/Player_Dealer_Correlation/roulette_dealer_finder_by_time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-cat $1_Dealer_schedule | awk -F" " '{print $1, $2, $5, $6}'| grep "$2"
-
-
-##The command below is how to run this script file, the date and time are changeable and will be
-##picked up by the internal variables.
-##sh roulette_dealer_finder_by_time.sh 0310 '02:00:00 PM'
